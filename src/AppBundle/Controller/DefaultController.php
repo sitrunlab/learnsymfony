ae271c4a8a5439e6d3ba7658eea2ff2eaa43120a
Abdul Malik Ikhsan <samsonasik@gmail.com>
2015-12-01 05:39:51 +0700

    psr7 support

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,20 +2,23 @@
 
 namespace AppBundle\Controller;
 
+use Psr\Http\Message\ServerRequestInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use Zend\Diactoros\Response\HtmlResponse;
 
 class DefaultController extends Controller
 {
     /**
      * @Route("/", name="homepage")
      */
-    public function indexAction(Request $request)
+    public function indexAction(ServerRequestInterface $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', array(
+        $renderView = $this->renderView('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
         ));
+
+        return new HtmlResponse($renderView);
     }
 }
